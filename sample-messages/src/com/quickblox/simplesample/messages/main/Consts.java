fb3134b99d984873ae2c267d9ffe9f6acd578c63
tatanka987 <tatanka.tvv@gmail.com>
2015-09-14 15:42:16 +0300

    - first commit to new branch

--- sample-messages/src/com/quickblox/simplesample/messages/main/Consts.java
+++ sample-messages/src/com/quickblox/simplesample/messages/main/Consts.java
@@ -3,11 +3,11 @@ package com.quickblox.simplesample.messages.main;
 public class Consts {
 
     // QuickBlox credentials
-    public static final int APP_ID = 99;
-    public static final String AUTH_KEY = "63ebrp5VZt7qTOv";
-    public static final String AUTH_SECRET = "YavMAxm5T59-BRw";
-    public static final String USER_LOGIN = "samsamsam";
-    public static final String USER_PASSWORD = "samsamsam";
+//    public static final int APP_ID = 99;
+//    public static final String AUTH_KEY = "63ebrp5VZt7qTOv";
+//    public static final String AUTH_SECRET = "YavMAxm5T59-BRw";
+//    public static final String USER_LOGIN = "samsamsam";
+//    public static final String USER_PASSWORD = "samsamsam";
 
     // In GCM, the Sender ID is a project ID that you acquire from the API console
     public static final String PROJECT_NUMBER = "761750217637";
